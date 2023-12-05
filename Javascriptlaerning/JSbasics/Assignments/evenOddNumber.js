3c657a1711d0b86c0b676649cf2634a6b019613c
“Deepshikha <deepshikha.pingle@gmail.com>
2023-12-05 11:56:17 +0530

    new data added

---
+++ Javascriptlaerning/JSbasics/Assignments/evenOddNumber.js
@@ -0,0 +1,11 @@
+let number = 225251
+{
+if ((number/2)*2==number){
+    console.log ("number is even")
+
+
+}
+else {
+    console.log ("number is odd")
+    
+}}
--- Javascriptlaerning/JSbasics/Assignments/primeanumber.js
+++ Javascriptlaerning/JSbasics/Assignments/primeanumber.js
@@ -1,14 +1,19 @@
-for (i=2; i<=21; i++ ){
-
-    if (i%2){
-    
-        console.log(i)
+function isprimenumber(n){
+if (n===0 || n===1) return false;   
+for (i=2; i <= n/2;i++){
+if (n % i == 0) return false;
+}
+{
+return true;
+}
+}
+let x = 100; 
+let first10PrimeNumbers = 0;
+    for (i=0; i <= x; i++ ){
+    if (isprimenumber()){
+        console.log(i);
+        first10PrimeNumbers++;
     }
-
-    else {
-
-        console.log("not prime")
-    }
-
-    
-    }
+    if (first10PrimeNumbers === 10){
+        break;
+    }}
